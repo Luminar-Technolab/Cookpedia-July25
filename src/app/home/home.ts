db15e7f3e863a6b60d04104c595bde4396f19208
soniagkalliath <soniageorge111@gmail.com>
2026-01-19 13:04:35 +0530

    ADD: API added for all recipes

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
 import { RouterLink } from "@angular/router";
+import { ApiService } from '../services/api-service';
 
 @Component({
   selector: 'app-home',
@@ -11,4 +12,18 @@ import { RouterLink } from "@angular/router";
 })
 export class Home {
 
+  allRecipes:any = signal([])
+  api = inject(ApiService)
+
+  ngOnInit(){
+    this.getAllRecipes()
+  }
+
+  getAllRecipes(){
+    this.api.getAllRecipeAPI().subscribe((res:any)=>{
+      // console.log(res);    
+      this.allRecipes.set(res.slice(0,6)) 
+      console.log(this.allRecipes());
+    })
+  }
 }
